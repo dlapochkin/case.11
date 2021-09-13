0838c3a05d47702cfbdc8f3795122f237aa35e3b
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-09-13 17:36:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ azs_status = dict()
 status = dict()
 while True:
     try:
-        line = input()
-        azs_info[line[0]] = [line[2], line[4:], 0]
+        line = list(input().split())
+        azs_info[line[0]] = [line[1], line[2:], 0]
         azs_status[line[0]] = []
         if line == '':
             break
     except (ValueError, EOFError):
         break
+
