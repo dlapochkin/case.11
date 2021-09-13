5ef97575dd457b0c0956ff7ba9bc0e668c8fdb13
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-09-13 20:30:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys, random
+sys.setrecursionlimit(1500)
 
 sys.stdin = open("azs.txt")
 azs_info = dict()
@@ -22,7 +23,7 @@ def check_place(azs_info, following):
     min_status = [0, 999]
     for key in azs_info:
         if benz in azs_info[key][1]:
-            if min_status[1] > azs_info[key][2] and azs_info[key][0] != azs_info[key][2]:
+            if min_status[1] > azs_info[key][2] and int(azs_info[key][0]) > azs_info[key][2]:
                 min_status[0] = key
                 min_status[1] = azs_info[key][2]
     if min_status[1] != 999:
@@ -32,7 +33,6 @@ def check_place(azs_info, following):
 
 
 def new(following, azs_status, petrol):
-    print(following)
     following.append(int(converter(following[0])) + following[3])
     azs_status[str(petrol)].append(following)
     return azs_status
@@ -40,26 +40,25 @@ def new(following, azs_status, petrol):
 
 def monitoring(azs_info, info, event):
     if event == 'departure':
-        print('В', info[4], 'клиент', info[0], info[1], info[2], info[3], 'заправил свой автомобиль и покинул очередь.')
+        print('В', converter(info[4]), 'клиент', info[0], info[1], info[2], info[3], 'заправил свой автомобиль и покинул очередь.')
     elif event == 'waste':
         print('что-то')  # дописать
     else:
         print('В', info[0], 'новый клиент:', info[0], info[1], info[2], info[3], 'встал в очередь к автомату №', event)
     for n in range(1,len(azs_info)+1):
         print('Автомат №{0} максимальная очередь: {1} Марки бензина:'.format(n, azs_info[str(n)][0]),
-            ','.join(azs_info[str(n)][1]))
+            ','.join(azs_info[str(n)][1]), azs_info[str(n)][2])
 
 
 
 def converter(time):
     if str(time).count(':'):
-        return str(int(time[:2]) * 60 + int(time[4:]))
+        return str(int(time[:2]) * 60 + int(time[3:]))
     else:
         return '{:02d}:{:02d}'.format(int(time) // 60, int(time) % 60)
 
 
 def modelling(following, azs_info, azs_status, status, time):
-    print(azs_info)
     petrol = None
     if time == 1440:
         return status
@@ -72,20 +71,24 @@ def modelling(following, azs_info, azs_status, status, time):
             following.append((int(following[1]) // speed) + 1)  # первая строка файла
     for n in range(1,len(azs_status)+1):
         if azs_status[str(n)] != []:
-            departure_time = converter(azs_status[str(n)][0][4])
-            if departure_time == str(time):
+            departure_time = str(azs_status[str(n)][0][4])
+            if departure_time <= str(time):
+                azs_info[str(n)][2] -= 1
                 monitoring(azs_info, azs_status[str(n)][0], 'departure')
                 azs_status[str(n)].pop()
     if str(time) == converter(following[0]):
         azs_info, petrol = check_place(azs_info,following)
-    if petrol == '-1':
+    if not petrol:
+        return modelling(following, azs_info, azs_status, status, time + 1)
+    elif petrol == '-1':
         monitoring(azs_info, following, 'waste')
         following = None
+        return modelling(following, azs_info, azs_status, status, time + 1)
     else:
         monitoring(azs_info, following, petrol)
-        azs_info = new(following, azs_status, petrol)
+        azs_status = new(following, azs_status, petrol)
         following = None
-    return modelling(following, azs_info, azs_status, status, time + 1)
+        return modelling(following, azs_info, azs_status, status, time + 1)
 
 
 sys.stdin = open('input.txt')
