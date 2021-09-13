4af4f00570ae18321f7cba8468bdecb0fe6ec193
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-09-13 17:27:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-
+import sys
+sys.stdin = open("azs.txt")
+azs_info = dict()
+azs_status = dict()
+status = dict()
+while True:
+    try:
+        line = input()
+        azs_info[line[0]] = [line[2], line[4:], 0]
+        azs_status[line[0]] = []
+        if line == '':
+            break
+    except (ValueError, EOFError):
+        break
