64dd526094de1048839cb492273a9e03098767df
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2021-09-13 17:18:53 +0700

    f

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-
+def main():
+    time(1)
+def time(t):
+    if t>1445:
+        return 0
+    with open('input.txt') as f_in:
+        print(new_client(f_in, time))
+    time(t+1)
+def new_client(f_in, time):
+    s = list(f_in.readline().split())
+    return s
+main()
